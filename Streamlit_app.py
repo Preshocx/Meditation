11757220022fc413851bc1c8d553d93004dbec22
Preshocx <109714203+Preshocx@users.noreply.github.com>
2023-05-20 01:05:03 -0400

    Create Streamlit_app.py

---
+++ Streamlit_app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from playsound import playsound
+
+def play_sound(sound_file):
+    playsound(sound_file)
+
+def main():
+    st.title("Meditation App")
+    st.write("Welcome to the Meditation App!")
+    st.write("Choose from the following options to relax and reduce stress:")
+
+    option = st.selectbox(
+        "Select an option",
+        ("Guided Meditation", "Calming Sounds", "Breathing Exercises")
+    )
+
+    if option == "Guided Meditation":
+        st.write("Sit comfortably, close your eyes, and listen to the guided meditation.")
+        st.write("Press the Play button to start the meditation.")
+        if st.button("Play"):
+            # TODO: Add code to play guided meditation audio file
+            st.write("Meditation is playing.")
+
+    elif option == "Calming Sounds":
+        st.write("Choose a calming sound to relax and unwind.")
+        sound_choice = st.selectbox(
+            "Select a sound",
+            ("Ocean Waves", "Rainforest", "Soft Piano")
+        )
+        if st.button("Play"):
+            if sound_choice == "Ocean Waves":
+                play_sound("ocean_waves.mp3")
+            elif sound_choice == "Rainforest":
+                play_sound("rainforest.mp3")
+            elif sound_choice == "Soft Piano":
+                play_sound("soft_piano.mp3")
+
+    elif option == "Breathing Exercises":
+        st.write("Practice deep breathing exercises to calm your mind and body.")
+        st.write("Inhale deeply through your nose, hold your breath for a few seconds, then exhale slowly through your mouth.")
+        if st.button("Start"):
+            # TODO: Add code for breathing exercise timer
+            st.write("Breathing exercise started.")
+
+if __name__ == '__main__':
+    main()
